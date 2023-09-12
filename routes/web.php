701a540193cec1aba4bcdab27dec53ad65d8c94a
jcbcouch <jcbcouch@gmail.com>
2023-09-12 17:53:55 -0500

    edit listing

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 
 Route::post('/listings', [ListingController::class, 'store']);
 
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
